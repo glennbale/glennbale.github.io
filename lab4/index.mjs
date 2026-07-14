6059652398ddf8ade299c9bcb076b7ce6484d2df
Glenn Bale <71362217glennbale@users.noreply.github.com>
2026-07-13 23:10:44 -0700

    lab 4 initial. Done with step 7

---
+++ lab4/index.mjs
@@ -0,0 +1,27 @@
+import express from "express";
+const planets = (await import("npm-solarsystem")).default;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/earth", (req, res) => {
+  let planetEarth = planets.getEarth();
+  console.log(planetEarth);
+  res.render("earth", { planetEarth });
+});
+
+app.get("/mars", (req, res) => {
+  let planetMars = planets.getMars();
+  console.log(planetMars);
+  res.render("mars", { planetMars });
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
